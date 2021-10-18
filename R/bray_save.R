294cf4c468ae153be99a31fa3666fbedef0501b2
Francesco Pascuzzi <fr.pscz@gmail.com>
2021-10-18 19:15:19 +0200

    Update bray_save.R

--- R/bray_save.R
+++ R/bray_save.R
@@ -1,3 +1,11 @@
+#' Save png output
+#'
+#' @param path Destination for png output
+#' @param plot ggplot2 plot object
+#' @param w width in inches (Default = \code{5})
+#' @param h height in inches (Default = \code{5})
+#' @param dpi density per inch (Default = \code{300})
+#' @return Nothing
 bray_save <- function(
   path,
   plot,
