bf6b3d5c64271377d4d757d2de1457ad12900bfb
Francesco Pascuzzi <fr.pscz@gmail.com>
2021-10-18 19:15:00 +0200

    Update end_bray.R

--- R/end_bray.R
+++ R/end_bray.R
@@ -1,3 +1,6 @@
+#' Switch back to ggplot2 defaults
+#'
+#' @return Nothing
 end_bray <- function(){
   ggplot2::theme_set(ggplot2::theme_gray())
   ggplot2::update_geom_defaults("point", list(color = "#000000"))
